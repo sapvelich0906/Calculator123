a9f5c2f306be5a09013d72c9aaa9e1cf7cbffa68
sapvelich0906 <sapvelich0906@mail.ru>
2016-10-28 02:47:41 +0600

    Create  calculator.hpp

---
+++ include/ calculator.hpp
@@ -0,0 +1,6 @@
+double sumary(double a, double b);
+double differense(double a, double b);
+double multiplication(double a, double b);
+double quotient(double a, double b);
+double power(double a, double b);
+double squareroot(double a);
