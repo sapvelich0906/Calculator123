9f49bbbac3c67766686ddd0a4c3cbd860ac9c6e4
sapvelich0906 <sapvelich0906@mail.ru>
2016-10-28 13:46:27 +0400

    Update and rename  main.cpp to main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "calculator.hpp"
+using namespace std;
+
+
+int main()
+{
+	double a, b;	
+	int c;	
+        cin >> a;		
+        cin >> b;	
+        cout << "c=";	
+        cin >> c;	
+        cout << roundp(sumary(a, b)) << endl;	
+        cout << roundp(differense(a, b)) << endl;	
+        cout << roundp(multiplication(a, b)) << endl;	
+        cout << roundp(quotient(a, b)) << endl;	
+        cout << roundp(power(a, c)) << endl;	
+        if (a >= 0)		
+                cout << squareroot(a) << endl;	
+        else		
+                cout << "koren izvlech nelzya" << endl;	
+return 0;
+}
