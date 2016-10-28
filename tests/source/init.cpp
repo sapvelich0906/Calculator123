73e1fd079553b16d9776e50f85d66ca6c0914e23
sapvelich0906 <sapvelich0906@mail.ru>
2016-10-28 14:27:51 +0400

    Create init.cpp

---
+++ tests/source/init.cpp
@@ -0,0 +1,52 @@
+#include <calculator.hpp>
+#include <catch.hpp>
+
+SCENARIO("calculator sumary", "[sumary]") {
+    float a = 16.0;
+    float b = 8.0;
+    int c = 3;
+    
+    double rv = sumary( a, b );
+    
+	REQUIRE( rv == 24.0 );
+}
+SCENARIO("calculator differense", "[differense]") {
+    double a = 16.0;
+    double b = 8.0;
+    
+    double rv = differense( a, b );
+    
+	REQUIRE( rv == 8.0 );
+}
+SCENARIO("calculator multiplication", "[multiplication]") {
+    double a = 16.0;
+    double b = 8.0;
+    
+    double rv = multiplication( a, b );
+    
+	REQUIRE( rv == 128.0 );
+}
+SCENARIO("calculator quotient", "[quotient]") {
+    double a = 16.0;
+    double b = 8.0;
+    
+    double rv = quotient( a, b );
+    
+	REQUIRE( rv == 2.0 );
+}
+SCENARIO("calculator power", "[power]") {
+    double a = 16.0;
+    double b = 8.0;
+    
+    double rv = power( a );
+    
+	REQUIRE( rv == 4096.0 );
+}
+SCENARIO("calculator squareroot", "[squareroot]") {
+    double a = 16.0;
+    double b = 8.0;
+    
+    double rv = squareroot( a );
+    
+	REQUIRE( rv == 4.0 );
+}
