5dbab49f822c83f26418e2ed82877ad37410b380
sapvelich0906 <sapvelich0906@mail.ru>
2016-11-09 02:56:55 +0600

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -2,7 +2,7 @@ double sumary(double a, double b);
 double differense(double a, double b);
 double multiplication(double a, double b);
 double quotient(double a, double b);
-double power(double a, double b);
+double power(double a, int c);
 double squareroot(double a);
 double absolut(double a);
 double roundp(double a);
