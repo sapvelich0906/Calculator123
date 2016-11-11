7b277700e9a8a5f24bf486b1bc2fb833319f6904
sapvelich0906 <sapvelich0906@mail.ru>
2016-11-11 14:57:06 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -26,11 +26,24 @@ double power(double a, int c)
         return a1;
 }
 double squareroot(double a)
-{	
-        double i = 0;	
-        while (i*i < a)		
-                i = i + 0.0001;
-        return i;
+{
+	double xn = 1.0;  
+	double xn1 = 2.0;
+
+	if ( a != 1 && a != 0 )
+	{
+		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
+		{
+		 xn = xn1;
+		 xn1 = (float)(xn + (float)a/xn)/2;
+		}
+
+	}
+		else if ( a == 1 ) xn1 = 1;
+			else if ( a == 0 ) xn1 = 0;
+
+
+	return xn1;
 }
 double absolut(double a)
 {	
