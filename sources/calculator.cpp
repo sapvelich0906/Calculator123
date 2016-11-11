facb7011af4fb695ee6150b9c4643d66f5665680
sapvelich0906 <sapvelich0906@mail.ru>
2016-11-11 13:30:32 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -29,7 +29,7 @@ double squareroot(double a)
 {	
         double i = 0;	
         while (i*i < a)		
-                i = i + 0.000000001;
+                i = i + 0.0000000001;
         return i;
 }
 double absolut(double a)
