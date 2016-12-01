9c7bbfa75712bae13948564bfdcf0617b6fb9808
sapvelich0906 <sapvelich0906@mail.ru>
2016-12-02 01:20:22 +0600

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -14,8 +14,14 @@ int main()
         cout << roundp(sumary(a, b)) << endl;	
         cout << roundp(differense(a, b)) << endl;	
         cout << roundp(multiplication(a, b)) << endl;
-	cout << roundp(quotient(a, b)) << endl;	
+	if (b==0) 
+		cout << "Error! Вivision by zero!" << endl;
+	else
+		cout << roundp(quotient(a, b)) << endl;	
         cout << roundp(power(a, c)) << endl;	
-        cout << squareroot(a) << endl;	       	
+	if (a>=0)
+		cout << squareroot(a) << endl;
+	else
+		cout << "Error! Square root of a negative number" << endl;	       	
 return 0;
 }
